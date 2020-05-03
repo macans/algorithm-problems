88e77f478cc500e7ebcf1196a17829739f45ab4b
Macan Zhang <jzhaa@amazon.com>
2020-05-03 13:37:39 +0800

    add problems

---
+++ archive/codeforces-1323C-Unusual Competitions.cpp
@@ -0,0 +1,60 @@
+/**
+ * @authors: https://github.com/macans
+ * @date: 2020-04-26 00:05:39
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <map>
+#include <stack>
+#include <set>
+#include <utility>
+#define ll long long
+#define INF 0x3f3f3f3f
+#define INFL 0x3f3f3f3f3f3f3f3fLL
+#define OPEN_FILE
+#define pp pair<int, int>
+#define MAXN 1000005
+using namespace std;
+
+string s;
+
+int main(){
+#ifdef OPEN_FILE
+    //freopen("in.txt", "r", stdin);
+#endif 
+    int n;
+    cin >> n;
+    cin >> s;
+    int ans = 0, cnt = 1, right = 0, left = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == '('){
+            ++left;
+        }
+        else{
+            --left;
+        }
+        if(left < 0){
+            ++cnt;
+        }
+        //cout << left << ", ";
+        if(left == 0 && cnt != 1){
+            ans += cnt;
+            cnt = 1;
+        }
+    }
+    if(left != 0){
+        cout << -1 << endl;
+    }
+    else{
+        cout << ans << endl;
+    }
+}
---
+++ archive/codeforces-1332D-Walk on Matrix .cpp
@@ -0,0 +1,43 @@
+/**
+ * @authors: https://github.com/macans
+ * @date: 2020-05-03 11:09:31
+ * 
+ * 矩阵构造
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <map>
+#include <stack>
+#include <set>
+#include <utility>
+#define ll long long
+#define INF 0x3f3f3f3f
+#define INFL 0x3f3f3f3f3f3f3f3fLL
+#define OPEN_FILE
+#define pp pair<int, int>
+#define MAXN 
+using namespace std;
+
+int main(){
+#ifdef OPEN_FILE
+	freopen("in.txt", "r", stdin);
+#endif 
+	int k;
+	int x = 1;
+	cin >> k;
+	while(x <= k){
+		x <<= 1;
+	}
+	cout << "2 3" << endl;
+	cout << x + k << " " << k << " " << 0 << endl;
+	cout << x << " " << x + k << " " << k << endl;
+}
