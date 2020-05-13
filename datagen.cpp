df60e34e25ac412659421a10beca2bb8de081f36
Macan Zhang <jzhaa@amazon.com>
2020-05-13 23:20:57 +0800

    add problems

--- datagen.cpp
+++ datagen.cpp
@@ -1,12 +1,36 @@
 #include <cstdio>
+#include <random>
+#include <iostream>
+#define MAXN 200000
 
 using namespace std;
 
+int prime[MAXN];
+bool isPrime[MAXN];
+int len;
+
+void get_prime(){
+	len = 0;
+	const int N = 200000;
+	memset(isPrime, true, sizeof(isPrime));
+	for(int i = 2; i <= N; ++i){
+		if(isPrime[i]){
+			prime[len++] = i;
+			for(int j= (i << 1); j <= N; j += i)
+				isPrime[j] = false;
+		}
+	}
+}
+
 int main(){
     freopen("in.txt", "w", stdout);
-    printf("1\n200000 1000000000 200000\n");
-    for(int i = 0; i < 200000; i++){
-        printf("%d ", 1);
-    };
+	get_prime();
+    int n = 200000;
+	n--;
+	cout << 10 << " ";
+    while(n--){
+        cout << 170777 << " ";
+    }
+    cout << endl;
     fclose(stdout);
 }
