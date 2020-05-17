8cb222bdea7e2c17248d85daf295162be7a169ce
Macan Zhang <jzhaa@amazon.com>
2020-05-18 03:17:55 +0800

    add problems

--- archive/Codeforces/codeforces-1353E-K-periodic Garland.cpp
+++ archive/Codeforces/codeforces-1353E-K-periodic Garland.cpp
@@ -1,53 +1,99 @@
-链接：https://www.nowcoder.com/questionTerminal/4060ac7e3e404ad1a894ef3e17650423?answerType=1&f=discussion
-来源：牛客网
+/**
+ * @authors: https://github.com/macans
+ * @date: 2020-05-15 00:09:02
+ * 
+ * dp[i] means the minimal cost if let s[i] be '1'
+ * then dp[i] = min(dp[i - k] + (s[i] != '1'), cntl[i - k] + (s[i] != '1')), cntl[i] means the count of '1' in the left and has same remainder when modulo k
+ * 
+ * res[i] means the minimal cost that let the string of which has same remainder with i%k be valid
+ * 
+ * res[i] = min(all values are '0', dp[i] + cntr[i + k])
+ * 
+ * the final answer will be 
+ * ans = min(res[i] + total '1' numbers - '1' numbers that has differemce remainder with i%k)
+ */
 
-class Solution {
-public:
-    void replaceSpace(char* str, int length) {
-        if(str == NULL || length <0)
-            return;
-        int num =0;    //空格数目
-        for (int i = 0; i < length; i++){
-            if (str[i] == ' ')
-                num++;
-        }
-        char* p1 = &str[length -1];//原字符串指针
-        char* p2 = &str[length -1 +num*2];//新字符串指针
-        for (int i = length-1; i >= 0; i--) {
-            if (str[i] == ' ') {
-                *p2-- = '0';
-                *p2-- = '2';
-                *p2-- = '%';
-                p1--;
-            }else{
-                *p2-- = *p1--;
-            }
-        }
-    }
-};
-class Solution {
-public:
-	void replaceSpace(char *str,int length) {
-        if(str == NULL || length == 0){
-            return;
-        }
-        int cnt = 0;
-        for(int i = 0; i < length; i++){
-            if(str[i] == ' '){
-                ++cnt;
-            }
-        }
-        char* p = &str[length - 1];
-        char* q = &str[length - 1 + (cnt << 1)];
-        for(int i = length - 1; i >= 0; i--){
-            if(str[i] == ' '){
-                *q-- = '0';
-                *q-- = '2';
-                *q-- = '%';
-                p--;
-            } else{
-                *q-- = *p--;
-            }
-        }
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <map>
+#include <stack>
+#include <set>
+#include <utility>
+#define ll long long
+#define INF 0x3f3f3f3f
+#define INFL 0x3f3f3f3f3f3f3f3fLL
+#define pp pair<int, int>
+#define MAXN 1000005
+using namespace std;
+
+string s;
+int cntl[MAXN], cntr[MAXN], cnt[MAXN];
+int dp[MAXN], res[MAXN];
+int main(){
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+#endif 
+	int t, n, k;
+	int first, last;
+	cin >> t;
+	while(t--){
+		cin >> n >> k;
+		cin >> s;
+		int sum = 0;
+		for(int i = 0; i < n; i++){
+			sum += (s[i] == '1');
+			cntl[i] = s[i] == '1';
+			if(i >= k){
+				cntl[i] += cntl[i - k];
+			}
+			cnt[i % k] = cntl[i];
+		}
+
+		for(int i = n - 1; i >= 0; i--){
+			cntr[i] = s[i] == '1';
+			if(i + k < n){
+				cntr[i] += cntr[i + k];
+			}
+		}
+		cntl[n] = cntr[n] = 0;		
+		for(int i = 0; i < k; i++){
+			dp[i] = (s[i] != '1');
+			if(i + k < n){
+				res[i] = min(cnt[i], dp[i] + cntr[i + k]);
+			}
+			else{
+				res[i] = min(cnt[i], dp[i]);
+			}
+			//printf("cnt[i]=%d, cntr[i+k]=%d, res[i]=%d\n", cnt[i], cntr[i+k], res[i]);
+
+		}
+		for(int i = k; i < n; i++){
+			//0 0 0 s[i]
+			dp[i] = cntl[i - k] + (s[i] != '1');
+			dp[i] = min(dp[i], dp[i - k] + (s[i] != '1'));
+			if(i + k < n){
+				res[i % k] = min(res[i % k], dp[i] + cntr[i + k]);
+			}
+			else{
+				res[i % k] = min(res[i % k], dp[i]);
+			}
+			//printf("i=%d, dp[i]=%d, res[i]=%d\n", i, dp[i], res[i]);
+
+		}
+		int ans = INF;
+		for(int i = 0; i < k; i++){
+			//printf("res[i]=%d, cnt[i]=%d\n", res[i], cnt[i]);
+			ans = min(ans, res[i] + sum - cnt[i]);
+		}
+		cout << ans << "\n";
+
 	}
-};
+} 
