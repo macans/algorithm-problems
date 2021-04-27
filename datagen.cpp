63b3b7a91a1d3a40a5af2461de05da721795bd66
zhangji05 <zhangji05@kuaishou.com>
2021-04-27 10:22:16 +0800

    add problems

--- datagen.cpp
+++ datagen.cpp
@@ -23,13 +23,18 @@ void get_prime(){
 }
 
 int main(){
-    freopen("in.txt", "w", stdout);
-	int n = 1000000, m = 100;
-	cout << n << " " << m << "\n";
-	while(n--){
-		cout << rand() % 1000000 - 500000 + 1 << " ";
+    //freopen("in.txt", "w", stdout);
+	int a[5][5] = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 0, 0, 0, 0};
+	int d[8] = {0};
+	int cnt = 0;
+    while(true){
+		int i = 5 * ((rand() % 5) - 1) + (rand() % 5);
+		if(i <= 21){
+			d[i % 7 + 1]++;
+			if(++cnt == 1000000) break;
+		}
 	}
-	cout << "\n";
-    cout << endl;
-    fclose(stdout);
+	for(int i = 1; i < 8; i++){
+		cout << i << " " << d[i] << "\n";
+	}
 }
