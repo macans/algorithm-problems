8fe14c68c071dc43841aee465dfcfe6b5ed5e4ca
zhangji05 <zhangji05@kuaishou.com>
2021-06-15 09:58:17 +0800

    add problems

---
+++ Codeforces/R724/C.java
@@ -0,0 +1,12 @@
+package Codeforces.R724;
+
+/**
+ * @author macinchang
+ * Created on  2021-06-14 23:08:34 
+ */
+public class C {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
