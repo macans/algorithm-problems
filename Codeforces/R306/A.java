1c8cc30ece832a498875d438bd3ea8809c432892
zhangji05 <zhangji05@kuaishou.com>
2021-06-22 10:13:40 +0800

    add problems

---
+++ Codeforces/R306/A.java
@@ -0,0 +1,23 @@
+package Codeforces.R306;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author macinchang
+ * Created on  2021-06-21 21:37:29 
+ */
+
+public class A {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = "AB", s2 = "BA";
+        List<Integer> p1 = new ArrayList<>();
+        while(scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+        }
+        scanner.close();  
+    }
+}
---
+++ Codeforces/RBeta/B.cpp
@@ -0,0 +1,9 @@
+/**
+ * @author macinchang
+ * Created on  2021-06-22 10:13:11 
+ */
+
+int main() {
+    
+    return 0;
+}
